99ae59f8c7f15a83e1d5c4a8244ee3e128994d94
Alex <alex.karlsson.001@gmail.com>
2019-11-19 12:21:12 +0100

    RED: Failing JUnit Card test (compiler error)

---
+++ code/src/test/java/CardTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CardTest {
+
+    @Test
+    public void createCardsTest() {
+        Card sut = new Card(Card.Color.CLUBS, Card.Value.TWO);
+        assertEquals(sut.getColor(), Card.Color.CLUBS);
+        assertEquals(sut.getValue(), Card.Value.TWO);
+    }
+}
