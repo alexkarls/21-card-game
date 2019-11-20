b7072947ffe5a0c97bb3a762a223e20ea7e9bba4
Alex <alex.karlsson.001@gmail.com>
2019-11-20 21:20:09 +0100

    REFACTOR: Added real implementation to check if winner, also to return winner when comparing two players. Updated tests to be more extensive and cover more scenarios/cases

--- code/src/main/java/model/Dealer.java
+++ code/src/main/java/model/Dealer.java
@@ -2,6 +2,7 @@ package model;
 
 public class Dealer {
 
+    private final int MAX_HAND = 5;
     private final int MAX_SCORE = 21;
 
     public boolean isLoser(Player player) {
@@ -9,10 +10,29 @@ public class Dealer {
     }
 
     public boolean isWinner(Player player) {
-        return true;
+        int score = player.getScore();
+        if (score == MAX_SCORE) {
+            return true;
+        }
+        return score < MAX_SCORE && player.size() == MAX_HAND;
     }
 
     public Player returnWinner(Player dealer, Player player) {
-        return dealer;
+        int playerScore = player.getScore();
+        int dealerScore = dealer.getScore();
+        if (playerScore > MAX_SCORE) {
+            return dealer;
+        }
+        if (dealerScore > MAX_SCORE) {
+            return player;
+        }
+        if (playerScore == dealerScore) {
+            return dealer;
+        }
+        if (playerScore < dealerScore) {
+            return dealer;
+        }
+        return player;
     }
 }
+
--- code/src/test/java/DealerTest.java
+++ code/src/test/java/DealerTest.java
@@ -5,16 +5,20 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DealerTest {
 
     private Dealer sut;
+    private Player dealer;
+    private Player player;
 
     @BeforeEach
     void setup() {
         sut = new Dealer();
+        dealer = new Player();
+        player = new Player();
     }
 
     @AfterEach
@@ -24,32 +28,62 @@ public class DealerTest {
 
     @Test
     void isLoserDealerTest() {
-        Player player = new Player();
-        player.add(new Card(Card.Suit.SPADES, Card.Rank.KING));
-        player.add(new Card(Card.Suit.HEARTS, Card.Rank.KING));
+        player.add(new Card(Card.Suit.CLUBS, Card.Rank.JACK));
+        assertFalse(sut.isLoser(player));
+        player.add(new Card(Card.Suit.DIAMONDS, Card.Rank.JACK));
+        // Added to better demonstrate the test/score (method tested in PlayerTest)
+        assertEquals(22, player.getScore());
         assertTrue(sut.isLoser(player));
     }
 
     @Test
-    void isWinnerDealerTest() {
-        Player player = new Player();
+    void isWinnerOnScoreDealerTest() {
+        player.add(new Card(Card.Suit.CLUBS, Card.Rank.TEN));
+        player.add(new Card(Card.Suit.DIAMONDS, Card.Rank.TEN));
+        assertEquals(20, player.getScore()); // Added to better demonstrate the score
+        assertFalse(sut.isWinner(player));
+        player.add(new Card(Card.Suit.CLUBS, Card.Rank.ACE));
+        assertEquals(21, player.getScore());
+        assertTrue(sut.isWinner(player));
+        player.add(new Card(Card.Suit.DIAMONDS, Card.Rank.ACE));
+        assertEquals(22, player.getScore());
+        assertFalse(sut.isWinner(player));
+    }
+
+    @Test
+    void isWinnerOnSizeDealerTest() {
         player.add(new Card(Card.Suit.CLUBS, Card.Rank.TWO));
         player.add(new Card(Card.Suit.DIAMONDS, Card.Rank.TWO));
-        player.add(new Card(Card.Suit.SPADES, Card.Rank.TWO));
         player.add(new Card(Card.Suit.HEARTS, Card.Rank.TWO));
+        player.add(new Card(Card.Suit.SPADES, Card.Rank.TWO));
+        assertFalse(sut.isWinner(player));
         player.add(new Card(Card.Suit.CLUBS, Card.Rank.THREE));
-        player.add(new Card(Card.Suit.DIAMONDS, Card.Rank.THREE));
-        // = 15 in score with 5 cards
         assertTrue(sut.isWinner(player));
     }
 
     @Test
-    void returnWinnerDealerTest() {
-        Player dealer = new Player();
-        Player player = new Player();
-        dealer.add(new Card(Card.Suit.SPADES, Card.Rank.TEN));
-        player.add(new Card(Card.Suit.CLUBS, Card.Rank.TWO));
+    void returnWinnerEqualDealerTest() {
+        dealer.add(new Card(Card.Suit.CLUBS, Card.Rank.TWO));
+        player.add(new Card(Card.Suit.DIAMONDS, Card.Rank.TWO));
         assertEquals(dealer, sut.returnWinner(dealer, player));
+    }
+
+    @Test
+    void returnWinnerCompareDealerTest() {
+        dealer.add(new Card(Card.Suit.CLUBS, Card.Rank.TEN));
+        player.add(new Card(Card.Suit.DIAMONDS, Card.Rank.JACK));
+        assertEquals(player, sut.returnWinner(dealer, player));
+        dealer.add(new Card(Card.Suit.CLUBS, Card.Rank.TWO));
+        assertEquals(dealer, sut.returnWinner(dealer, player));
+    }
+
+    @Test
+    void returnWinnerBustDealerTest() {
+        dealer.add(new Card(Card.Suit.CLUBS, Card.Rank.JACK));
+        dealer.add(new Card(Card.Suit.DIAMONDS, Card.Rank.JACK));
+        player.add(new Card(Card.Suit.CLUBS, Card.Rank.TWO));
+        assertEquals(22, dealer.getScore());
+        assertEquals(player, sut.returnWinner(dealer, player));
     }
 
 }
