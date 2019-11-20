78b57537b72cd896585141736f8958c87890478f
Alex <alex.karlsson.001@gmail.com>
2019-11-20 10:51:58 +0100

    REFACTOR: Player has real implementation to calculate score. Made the Player tests more expansive (e.g. test with more than one card)

--- code/src/main/java/model/Player.java
+++ code/src/main/java/model/Player.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class Player {
 
+    private final int MAX_SCORE = 21;
+    private final int CARD_VALUE_OFFSET = 2;
     List<Card> hand;
 
     public Player() {
@@ -15,16 +17,27 @@ public class Player {
         hand.add(c);
     }
 
-    public int size() {
-        return hand.size();
-    }
-
     public Iterable<Card> getHand() {
         return hand;
     }
 
     public int getScore() {
-        return 10;
+        int score = 0;
+        for (Card c : hand) {
+            score = score + c.getRank().ordinal() + CARD_VALUE_OFFSET;
+        }
+        if (score > MAX_SCORE) {
+            for (Card c : hand) {
+                if (c.getRank() == Card.Rank.ACE && score > MAX_SCORE) {
+                    score = score - (c.getRank().ordinal() + (CARD_VALUE_OFFSET - 1));
+                }
+            }
+        }
+        return score;
+    }
+
+    public int size() {
+        return hand.size();
     }
 
 }
--- code/src/test/java/PlayerTest.java
+++ code/src/test/java/PlayerTest.java
@@ -1,8 +1,10 @@
 import model.Card;
 import model.Deck;
 import model.Player;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -16,20 +18,40 @@ public class PlayerTest {
         sut = new Player();
     }
 
+
+    @AfterEach
+    void teardown(TestInfo testInfo) {
+        System.out.println("Done with test: " + testInfo.getDisplayName());
+    }
+
     @Test
     void addCardPlayerTest() {
-        Card c = new Card(Card.Suit.HEARTS, Card.Rank.TEN);
-        sut.add(c);
+        Card[] cards = new Card[]{new Card(Card.Suit.CLUBS, Card.Rank.TWO),
+                new Card(Card.Suit.HEARTS, Card.Rank.ACE),
+                new Card(Card.Suit.SPADES, Card.Rank.ACE)};
+        for (Card c : cards) {
+            sut.add(c);
+        }
+        int count = 0;
         for (Card cardInHand : sut.getHand()) {
-            assertTrue(c.equals(cardInHand));
+            assertTrue(cards[count].equals(cardInHand));
+            count++;
         }
     }
 
     @Test
     void handScorePlayerTest() {
-        Card c = new Card(Card.Suit.HEARTS, Card.Rank.TEN);
-        sut.add(c);
-        assertEquals(10, sut.getScore());
+        sut.add(new Card(Card.Suit.CLUBS, Card.Rank.TWO));
+        assertEquals(2, sut.getScore());
+        sut.add(new Card(Card.Suit.HEARTS, Card.Rank.ACE));
+        assertEquals(16, sut.getScore());
+        /*
+        Ace that makes score > MAX_SCORE is valued at 1
+        See Player method for implementation details
+         */
+        sut.add(new Card(Card.Suit.SPADES, Card.Rank.ACE));
+        assertEquals(17, sut.getScore());
+
     }
 
     @Test
@@ -37,5 +59,7 @@ public class PlayerTest {
         Deck deck = new Deck(new MockRandom());
         sut.add(deck.draw());
         assertEquals(1, sut.size());
+        sut.add(deck.draw());
+        assertEquals(2, sut.size());
     }
 }
